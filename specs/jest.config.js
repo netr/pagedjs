73f0e234cf47c347fd0c49cbd62811c1b31d6427
Tommie Gannert <tommie@gannert.se>
2025-03-24 23:50:38 +0100

    Enables Typescript for spec tests.

--- specs/jest.config.js
+++ specs/jest.config.js
@@ -1,15 +1,15 @@
 export default {
-	testMatch: ["**/?(*.)(spec).js?(x)"],
+	testMatch: ["**/?(*.)(spec).[jt]s?(x)"],
 	globalSetup: "./jest_helpers/setup.js",
 	globalTeardown: "./jest_helpers/teardown.js",
 	testEnvironment: "./jest_helpers/puppeteer_environment.js",
 	setupFilesAfterEnv: ["./jest_helpers/setup_tests.js"],
 	transform: {
-		"\\.js$": ["babel-jest", { configFile: "./babel-jest.config.json" }]
+		".*\\.[jt]s$": ["babel-jest", { configFile: "./babel-jest.config.json" }],
 	},
 	transformIgnorePatterns: [
 		// TODO: babel is inserting require functions into ES modules.
-		"specs/jest_helpers/(setup|teardown|puppeteer_environment)\\.js$",
+		"specs/jest_helpers/(setup|teardown|puppeteer_environment)\\.[jt]s$",
 		// TODO: Why does playwright's serialize function end up with an undefined used of _typeof2?
 		// Seems to be something that's in babel's runtime library.
 		"node_modules/playwright-core/.*",
